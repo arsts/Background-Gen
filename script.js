538e89d114645d06508f0cd8c4cca4b185d9bc6c
arsts <arstsu@gmail.com>
2019-04-20 16:28:36 +0300

    added gradient h3

--- script.js
+++ script.js
@@ -11,8 +11,18 @@ function setGradient() {
     + ", "
     + color2.value
     + ")";
+    console.log(css);
+    
+    css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+
+// 1. Write code so that the colour inputs match the background generated on the first page load. 
+
+// 2. Display the initial CSS linear gradient property on page load.
+
+// 3. BONUS: Add a random button which generates two random numbers for the colour inputs.
